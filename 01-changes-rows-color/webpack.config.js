724bb38cba24c58690a83dcab54309d207749b0a
chengzhnegzhou <1976644720@qq.com>
2023-04-08 13:40:13 +0800

    指定webpack的entry和output

--- 01-changes-rows-color/webpack.config.js
+++ 01-changes-rows-color/webpack.config.js
@@ -1,3 +1,10 @@
+const path = require('path')
 module.exports = {
-    mode:'development'
+    // development production
+    mode:'development',
+    entry:path.join(__dirname,'./src/index.js'),
+    output:{
+        path:path.join(__dirname,'dist'),
+        filename:'bundle.js'
+    }
 }
