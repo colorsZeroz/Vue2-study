d3258bfe8de71ce5eafe349f29bc9ba21a09a111
chengzhnegzhou <1976644720@qq.com>
2023-04-12 13:07:59 +0800

    配置加载图片的loader

--- 01-changes-rows-color/src/index.js
+++ 01-changes-rows-color/src/index.js
@@ -1,7 +1,11 @@
 import $ from 'jquery'
 import './css/index.css'
+import './css/index.less'
+import logo from './images/backiee-67970.jpg'
 
+$('.box').attr('src',logo)
+// console.log(logo);
 $(function(){
-    $('li:odd').css('background-color','blue')
+    $('li:odd').css('background-color','aqau')
     $('li:even').css('background-color','pink')
 })
